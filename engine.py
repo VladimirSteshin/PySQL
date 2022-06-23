58a3d9615b3feaa240d5f9f16f299341043df3ca
Vladimir Steshin <warheim24@gmail.com>
2022-06-23 13:07:24 +0300

    Last left

--- engine.py
+++ engine.py
@@ -8,60 +8,111 @@ class Customer:
                                      password=self.keys['password'])
         self.curs = self.conn.cursor()
 
+    def clean_before(self):
+        self.curs.execute("""
+            DROP TABLE IF EXISTS phonebook
+            """)
+        self.curs.execute("""
+            DROP TABLE IF EXISTS customer
+            """)
+        print('Done')
+
     def create_db(self):
         self.curs.execute("""
-                CREATE TABLE IF NOT EXISTS customer (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(60) NOT NULL,
-                last_name VARCHAR (60) NOT NULL,
-                email VARCHAR(60)
-                );
-                """)
+            CREATE TABLE IF NOT EXISTS customer (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(60) NOT NULL,
+            last_name VARCHAR (60) NOT NULL,
+            email VARCHAR(60)
+            );
+            """)
         self.curs.execute("""
-                CREATE TABLE IF NOT EXISTS phonebook (
-                id SERIAL PRIMARY KEY,
-                customer_id INTEGER REFERENCES customer(id),
-                phone_number VARCHAR(60)
-                );
-                """)
+            CREATE TABLE IF NOT EXISTS phonebook (
+            id SERIAL PRIMARY KEY,
+            customer_id INTEGER REFERENCES customer(id),
+            phone_number VARCHAR(60)
+            );
+            """)
 
     def add_customer(self, name, last_name):
         self.curs.execute("""
-        INSERT INTO customer (name, last_name)
-        VALUES (%s, %s);
-        """, (name, last_name))
+            INSERT INTO customer (name, last_name)
+            VALUES (%s, %s);
+            """, (name, last_name))
         print('Customer added!')
 
     def add_phone(self, name, last_name, phone_number):
         self.curs.execute("""
-        SELECT id FROM customer
-        WHERE name = (%s) and last_name = (%s);
-        """, (name, last_name))
+            SELECT id FROM customer
+            WHERE name = %s and last_name = %s;
+            """, (name, last_name))
         cust_id = self.curs.fetchone()
         if cust_id is None:
-            return print('There is no such a person! Try again.')
+            print('There is no such a person! Restart the program with correct data.')
         else:
             cust_id = cust_id[0]
             self.curs.execute("""
-            INSERT INTO phonebook (customer_id, phone_number)
-            VALUES (%s, %s);
-            """, (cust_id, phone_number))
+                INSERT INTO phonebook (customer_id, phone_number)
+                VALUES (%s, %s);
+                """, (cust_id, phone_number))
+            print('Phone number added!')
 
     def update(self, old_name, old_last_name, new_name, new_last_name, email):
         self.curs.execute("""
-                SELECT id FROM customer
-                WHERE name = (%s) and last_name = (%s);
-                """, (old_name, old_last_name))
+                    SELECT id FROM customer
+                    WHERE name = %s and last_name = %s;
+                    """, (old_name, old_last_name))
         cust_id = self.curs.fetchone()
         if cust_id is None:
-            return print('There is no such a person! Try again.')
+            print('There is no such a person! Restart the program with correct data.')
         else:
             cust_id = cust_id[0]
             self.curs.execute("""
-            UPDATE customer
-            SET name = (%s), last_name = (%s), email = (%s)
-            WHERE id = (%s);
-            """, (new_name, new_last_name, email, cust_id))
+                UPDATE customer
+                SET name = %s, last_name = %s, email = %s
+                WHERE id = %s;
+                """, (new_name, new_last_name, email, cust_id))
+            print('Updated!')
+
+    def del_phone(self, name, last_name, phone_number):
+        self.curs.execute("""
+            SELECT id FROM customer
+            WHERE name = %s and last_name = %s;
+            """, (name, last_name))
+        cust_id = self.curs.fetchone()
+        if cust_id is None:
+            print('There is no such a person! Restart the program with correct data.')
+        else:
+            cust_id = cust_id[0]
+            self.curs.execute("""
+                DELETE FROM phonebook
+                WHERE customer_id = %s and phone_number = %s;
+                """, (cust_id, phone_number))
+            print('Number deleted!')
+
+    def del_person(self, name, last_name):
+        self.curs.execute("""
+                    SELECT id FROM customer
+                    WHERE name = %s and last_name = %s;
+                    """, (name, last_name))
+        cust_id = self.curs.fetchone()
+        if cust_id is None:
+            print('There is no such a person! Restart the program with correct data.')
+        else:
+            cust_id = cust_id[0]
+            self.curs.execute("""
+                    DELETE FROM phonebook
+                    WHERE customer_id = %s;
+                    """, (cust_id,))
+            self.curs.execute("""
+                    DELETE FROM customer
+                    WHERE id = %s;
+                    """, (cust_id,))
+            print('Person deleted!')
+
+    # def find(self, name, last_name, phone_number, email):
+    #     if phone_number is not None:
+    #         self.curs
 
     def shutdown(self):
         self.conn.commit()
--- main.py
+++ main.py
@@ -13,13 +13,22 @@ def keys():
 
 
 def user_task(task=input('Input command: ').lower()):
-    tasks = ['add_person', 'add_phone', 'update', 'del_phone', 'del_person', 'find', 'exit']
+    tasks = ['add_person', 'add_phone', 'update', 'del_phone', 'del_person', 'find', 'exit', 'clean']
     while True:
         if task not in tasks:
             print('Wrong command! Try again.')
             return user_task(task=input('Input command: ').lower())
         elif task == 'exit':
             return print('Have a nice day!')
+        elif task == 'clean':
+            print('It will delete all data. Are you sure? Y/N and press Enter:')
+            decision = str(input()).upper()
+            if decision == 'Y':
+                return tabling.clean_before()
+            elif decision == 'N':
+                return print('Canceled')
+            else:
+                print('Incorrect command.')
         else:
             if task == 'add_person':
                 return tabling.add_customer('John', 'Doe')
@@ -27,6 +36,10 @@ def user_task(task=input('Input command: ').lower()):
                 return tabling.add_phone('John', 'Doe', '123-45-67')
             elif task == 'update':
                 return tabling.update('John', 'Doe', 'Jack', 'Slater', 'schwarz@big.com')
+            elif task == 'del_phone':
+                return tabling.del_phone('Jack', 'Slater', '123-45-67')
+            elif task == 'del_person':
+                return tabling.del_person('Jack', 'Slater')
 
 
 tabling = Customer(keys())
