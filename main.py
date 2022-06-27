e5ce996b0d6b7dd68793e6035e9b7857352243eb
Steshin <@gs.pmok>
2022-06-27 17:00:23 +0300

    Done?

--- main.py
+++ main.py
@@ -49,6 +49,11 @@ def add_phone(conn, client_id, phone):
 def change_client(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
     curs = conn.cursor()
     curs.execute("""
+        SELECT COUNT(*) FROM phonebook
+        WHERE customer_id = %s;
+    """, (client_id,))
+    phones = curs.fetchone()[0]
+    curs.execute("""
         UPDATE customer
         SET first_name = %s, last_name = %s, email =%s, phones = %s
         WHERE id = %s;
@@ -62,7 +67,7 @@ def delete_phone(conn, client_id, phone):
     curs = conn.cursor()
     curs.execute("""
         DELETE FROM phonebook
-        WHERE customer_id = %s and phone = %s';
+        WHERE customer_id = %s and phone = %s;
         """, (client_id, phone))
     conn.commit()
     curs.close()
@@ -88,28 +93,28 @@ def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
     curs = conn.cursor()
     if phone is not None:
         curs.execute("""
-        SELECT * FROM customer c
-        JOIN phonebook p ON c.id = p.customer_id
-        WHERE phone = %s;
-        """, (phone,))
+            SELECT * FROM customer c
+            JOIN phonebook p ON c.id = p.customer_id
+            WHERE phone = %s;
+            """, (phone,))
     elif email is not None:
         curs.execute("""
-        SELECT * FROM customer c
-        JOIN phonebook p ON c.id = p.customer_id
-        WHERE email = %s;
-        """, (email,))
+            SELECT * FROM customer c
+            JOIN phonebook p ON c.id = p.customer_id
+            WHERE email = %s;
+            """, (email,))
     elif last_name is not None:
         curs.execute("""
-        SELECT * FROM customer c
-        JOIN phonebook p ON c.id = p.customer_id
-        WHERE last_name = %s;
-        """, (last_name,))
+            SELECT * FROM customer c
+            JOIN phonebook p ON c.id = p.customer_id
+            WHERE last_name = %s;
+            """, (last_name,))
     elif first_name is not None:
         curs.execute("""
-        SELECT * FROM customer c
-        JOIN phonebook p ON c.id = p.customer_id
-        WHERE first_name = %s;
-        """, (first_name,))
+            SELECT * FROM customer c
+            JOIN phonebook p ON c.id = p.customer_id
+            WHERE first_name = %s;
+            """, (first_name,))
     print(curs.fetchall())
     conn.commit()
     curs.close()
@@ -128,6 +133,36 @@ def clean_db(conn):
     print('Done')
 
 
+def get_client_id_list(conn):
+    curs = conn.cursor()
+    customers = []
+    curs.execute("""
+        SELECT id FROM customer;
+        """)
+    for client_id in curs.fetchall():
+        customers.append(client_id[0])
+    conn.commit()
+    curs.close()
+    return customers
+
+
+def upd_phones_count(conn, client_list):
+    curs = conn.cursor()
+    for client_id in client_list:
+        curs.execute("""
+            SELECT COUNT(*) FROM phonebook
+            WHERE customer_id = %s;
+            """, (client_id,))
+        phones = curs.fetchone()[0]
+        curs.execute("""
+            UPDATE customer
+            SET phones = %s
+            WHERE id = %s;
+            """, (phones, client_id))
+    conn.commit()
+    curs.close()
+
+
 with psycopg2.connect(database="customer", user="postgres", password="120290Vova") as conn:
     clean_db(conn)
     create_db(conn)
@@ -137,11 +172,19 @@ with psycopg2.connect(database="customer", user="postgres", password="120290Vova
     add_phone(conn, '1', '051-404')
     add_phone(conn, '3', '666-13-13')
     add_phone(conn, '2', '777-9-777')
-    change_client(conn, '1', 'Italian', 'Stallion', 'rocky@porn.com')
+    add_phone(conn, '2', '456-5-098')
+    add_phone(conn, '2', '321-1-123')
+    add_phone(conn, '1', '99999999')
+    delete_phone(conn, '1', '051-404')
+    change_client(conn, '1', 'Italian', 'Stallion', 'rocky@dredd.com')
+    upd_phones_count(conn, '1')
+    upd_phones_count(conn, '2')
+    upd_phones_count(conn, '3')
     find_client(conn, 'Italian')
     find_client(conn, None, None, 'austin@spy.com')
     find_client(conn, None, None, None, '666-13-13')
+    delete_client(conn, '3')
+    get_client_id_list(conn)
+    upd_phones_count(conn, get_client_id_list(conn))
 
 conn.close()
-
-# Сделать так, чтобы телефоны прибавлялись, отбавлялись, обновлялось кол-во
